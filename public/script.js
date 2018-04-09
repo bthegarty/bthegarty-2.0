daf208bc3b67b522908f4ae558f5c2c437ce9013
bthegarty <brendan.hegarty1@marist.edu>
2018-04-09 17:34:36 -0400

    styling
    -added sass and accompanying files
    --need to adjust colors and wording

---
+++ public/script.js
@@ -0,0 +1,35 @@
+// Start scroll
+$(function(){
+  var header =  $('.navbar');
+
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if(scroll >= 50){
+      header.addClass('scrolled');
+    }
+    else if(scroll < 50){
+      header.removeClass('scrolled');
+    }
+    else{
+      hearder.removeClass('scrolled');
+    }
+  });
+});
+// drop scroll
+$(function(){
+  var header =  $('.dropbtn');
+
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if(scroll >= 50){
+      header.addClass('scrolled-drop');
+    }
+    else if(scroll < 50){
+      header.removeClass('scrolled-drop');
+    }
+    else{
+      hearder.removeClass('scrolled-drop');
+    }
+  });
+});
+// end scroll
