3d04e76830be6070d1c1f7ce0078b9fc05eadfc5
bthegarty <brendan.hegarty1@marist.edu>
2018-04-30 17:41:26 -0400

    Career page
    -added styling and images for career page
    -added foundation for particles js
    -cleaned some useless scroll scripts

--- public/script.js
+++ public/script.js
@@ -33,12 +33,10 @@ $(function(){
   });
 });
 // end scroll
-// smooth scroll
-$('.smooth').on('click', function() {
-    $.smoothScroll({
-        scrollElement: $('body'),
-        scrollTarget: '#' + this.id
-    });
-
-    return false;
+//scroll animation
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
 });
