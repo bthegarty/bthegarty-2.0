3a21f11214f68b2c6f9b0667e8207efa7422c74d
bthegarty <brendan.hegarty1@marist.edu>
2018-06-13 11:38:30 -0400

    correctly call port var

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.use('/not-found',function(req, res){
   res.send("Error 404: Well golly, I'm not really sure how you got here");
 });
 
-app.listen(8080,function(){
+app.listen(port,function(){
   console.log('Server running at Port 8080');
 });
