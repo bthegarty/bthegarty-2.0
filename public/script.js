bf4963b924afe0312b711be8d7bc597095fcaab2
bthegarty <brendan.hegarty1@marist.edu>
2019-05-06 17:34:30 -0400

    page transition adjustments and temp job tiles

--- public/script.js
+++ public/script.js
@@ -3,30 +3,13 @@ const transition = () => {
 
   function toggle (e) {
     const target = e.currentTarget
-    const page1 = document.getElementById('js-pt-page-1')
-    const page2 = document.getElementById('js-pt-page-2')
-    page1.classList.toggle('hidden')
-    page2.classList.toggle('reveal')
+    const page1 = document.getElementById('js-pt-page')
+    page1.classList.toggle('page-2')
   }
 
   button.addEventListener('click', toggle)
 }
 
-const backButton = () => {
-  const back = document.getElementById('js-back')
-
-  function toggle (e) {
-    const target = e.currentTarget
-    const page1 = document.getElementById('js-pt-page-1')
-    const page2 = document.getElementById('js-pt-page-2')
-    page1.classList.toggle('reveal')
-    page2.classList.toggle('hidden')
-  }
-
-  back.addEventListener('click', toggle)
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   transition()
-  backButton()
 })
