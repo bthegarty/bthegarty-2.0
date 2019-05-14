cdfa276b39ddc96ea8a9aa2255a94ce40b96c8a1
bthegarty <brendan.hegarty1@marist.edu>
2019-05-14 17:39:35 -0400

    data-attributes

--- public/script.js
+++ public/script.js
@@ -11,14 +11,35 @@ const transition = () => {
 }
 
 const openTab = (e, tabName) => {
-  const tabContent = document.getElementsByClassName('tabContent')
-  for(let i = 0; i < tabContent.length; i++) {
-    // tabContent[i].style.display = 'none'
+  const tabButton = document.getElementsByClassName('tabButton')
+  const tabContainer = document.getElementById('js-tab-container')
+  const tabContent = tabContainer.getElementsByClassName('tabContent')
+  for (let i = 0; i < tabButton.length; i++) {
+    tabButton[i].addEventListener('click', function() {
+    let current = document.getElementsByClassName('active')
+    current[0].className = current[0].className.replace(' active', '')
+    this.className += " active";
+  })
   }
-  document.getElementById(tabName).style.display = 'flex'
+}
+
+const openTab2 = () => {
+  const tabButton = document.getElementsByClassName('js-tabButton')
+  const id = 1
+
+  function active (e) {
+    const target = e.currentTarget
+    const tabContent = document.querySelector('div.tabContent[data-cat-id="' + id + '"]')
+    console.log('CON =', tabContent)
+    // tabContent.classList.toggle('active')
+  }
+
+  for (let i = 0; i < tabButton.length; i++) {
+    tabButton[i].addEventListener('click', active)
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   transition()
-  openTab()
+  openTab2()
 })
